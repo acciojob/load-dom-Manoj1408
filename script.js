c0069ae5180c40a4728f34c05f66ac12f903e936
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-22 15:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+      var dta = document.getElementById('data');
+      dta.innerText = 'DOM load success'
+  
+});
